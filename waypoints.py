4634be701dd5a7d5a7af23489fd3f21e0393cea8
Code-Parth <softsparths@gmail.com>
2022-07-23 09:40:41 +0530

    Add Files

---
+++ waypoints.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Name Of your Map
+track_name = "AWS_track"
+
+# Folder Path Where Your .NPY Files are saved
+absolute_path = "./Maps-Waypoints-NPY-Files/"
+
+waypoints = np.load("%s/%s.npy" % (absolute_path, track_name))
+
+print("Number of waypoints = " + str(waypoints.shape[0]))
+for i, point in enumerate(waypoints):
+    waypoint = (point[2], point[3])
+    plt.scatter(waypoint[0], waypoint[1])
+    print("Waypoint " + str(i) + ": " + str(waypoint))
+
+plt.show()
